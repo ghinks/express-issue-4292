21776ac7a79e39b33102b3e83c78459b52341807
Glenn Hinks <ghinks@yahoo.com>
2020-05-24 09:31:46 -0400

    bug: 4292 download example

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const app = express();
+const fs = require('fs');
 const port = 3000;
 
 const routeHandlerOne = (req, res) => {
   console.log("example handler 1");
-  res.send("route handler 1")
+  const file = `${__dirname}/data.csv`;
+  const data = fs.readFileSync(file, 'utf-8');
+  console.log(`the data is ${data}`)
+  res.download(file, 'the-test-file.csv', (err) => {
+    if (err) {
+      res.status(500).send('error');
+    }
+    else {
+      console.log('file was downloaded')
+    }
+  })
 };
-
-
 
 app.get("/api/download/csv", routeHandlerOne);
 
